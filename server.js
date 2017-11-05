5978c417bdd3a7e29e3ff3936d3369f09d6945b7
Bill Sorrentino <williamsorrentino@gmail.com>
2017-11-05 09:43:34 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
+
+
+// Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var app = express();
 
+//Set up the express app
+var app = express();
 var PORT = process.env.PORT || 3000;
 
+//Requiring models for syncing
+var db = require('./models');
 
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Serve static content for the app from the "public" directory in the application directory.
+// Static directory
 app.use(express.static("public"));
-
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -27,8 +34,10 @@ var routes = require("./controllers/triviaController.js");
 
 app.use("/", routes);
 
-//app.listen(port);
-
-app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
