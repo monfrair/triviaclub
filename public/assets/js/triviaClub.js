745f9174327efb4906f52cf1d8b5916bf97ba933
Kyle Hoernlein <khoernlein90@gmail.com>
2017-11-11 10:50:42 -0500

    updated js

--- public/assets/js/triviaClub.js
+++ public/assets/js/triviaClub.js
@@ -1,24 +1,40 @@
 $(document).ready(function() {
+    var questionArray = [];
+    var answerArray = [];
 
-var categoryInput = $("#trivia-well").val()
+    $("#table").on("change", function() {
+        var categoryID = this.value;
+        console.log(categoryID)
+        questionArray = [];
+        answerArray = [];
 
-    function getQuestions() {
-        $.get("/api/questions", function(data) {
-            var rowsToAdd = [];
-            for (var i = 0; i < data.length; i++) {
-                // rowsToAdd.push(createQuestionRow(data[i]));
-                console.log(data[i].question);
-                // var triviaQuestion = $("<p>");
-                // triviaQuestion.text(data[i].question)
-                $(".triviaQuestion").append("<h2>" + data[i].question + "</h2>")
-            }
-            //   renderAuthorList(rowsToAdd);
-            //   nameInput.val("");
-        });
-    }
-    getQuestions()
-        // Function for creating a new list row for authors
-        // function createAuthorRow(questionData) {
-        //   console.log(questionData);
-    // }
+        function getQuestions() {
+
+            $.get("/api/questions/category/" + categoryID, function(data) {
+                for (var i = 0; i < data.length; i++) {
+                    console.log(data[i])
+                    var pickedQuestion = data[i].question;
+                    var correctAnswer = data[i].correct_answer;
+                    var inncorrectOne = data[i].incorrect_answers__001;
+                    var inncorrectTwo = data[i].incorrect_answers__002;
+                    var inncorrectThree = data[i].incorrect_answers__003;
+                    // console.log(pickedQuestion);
+                    questionArray.push(pickedQuestion)
+                    answerArray.push(inncorrectOne)
+                    answerArray.push(inncorrectTwo)
+                    answerArray.push(inncorrectThree)
+                }
+                    for (var i = 0; i < questionArray.length; i++) {
+                        console.log(questionArray[i])
+                    $(".triviaQuestion").append("<h3>" + questionArray[i] + "</h3>")
+
+                    }
+            });
+        }
+        getQuestions()
+    })
+
+    // $("#submitBtn").on("click", function(){
+    //     getQuestions()
+    // })
 });
